a9ba84b86cf1e7c289cc7dd0a0cddca9db6ff011
Mr Heaven <121832977+Mrheaven778@users.noreply.github.com>
2024-05-22 20:28:22 +0200

    Update axios.ts

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: 'https://uptask-api-production.up.railway.app/api/',
+    
     // baseURL: 'http://localhost:4000/api/',
 });
 
