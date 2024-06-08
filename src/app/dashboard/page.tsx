60d89dae83bb351f55b83e7ed6acfa08d14b6e2e
Mr Heaven <121832977+Mrheaven778@users.noreply.github.com>
2024-06-08 21:12:40 +0200

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,13 +5,16 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export default function pageDashboard() {
+export default function PageDashboard() {  // Cambié el nombre aquí
   const { isAuthenticated } = useAuth();
   const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     setIsLoading(false);
   }, []);
+
   const router = useRouter();
+
   if (isLoading) {
     return (
       <div className="flex justify-center items-center h-screen">
@@ -19,19 +22,21 @@ export default function pageDashboard() {
       </div>
     );
   }
+
   if (!isAuthenticated) {
     router.push("/auth/login");
   }
+
   return (
     <div>
-      <h1 className="text-5xl font-black text-white">Mis proyectos</h1>{" "}
+      <h1 className="text-5xl font-black text-white">Mis proyectos</h1>
       <p className="text-2xl font-light text-gray-300 mt-5">
         Maneja y organiza tus proyectos de manera sencilla
       </p>
       <nav className="my-6">
         <Link
           href="/dashboard/project/create"
-          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors rounded-xl "
+          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors rounded-xl"
         >
           Nuevo proyecto
         </Link>
